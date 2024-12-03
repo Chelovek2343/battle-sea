8545ae6700d0a3d2350f9802f9bf2bff1cf78ff3
Ulugbek <79435067+Chelovek2343@users.noreply.github.com>
2024-12-03 14:53:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import random, os
 
-def clearTerminal(): os.system("cls")
+def clear(): os.system("cls" if os.name == "nt" else "clear")
 
 def print_board(board):
     print("  " + " ".join(map(str, range(1, len(board[0]) + 1))))
     print("\n".join(f"{chr(65 + i)} " + " ".join(row) for i, row in enumerate(board)))
 
 def is_empty(hidden, r, c, size, o):
-    return all(0 <= r + o[0] * i < len(hidden) and 0 <= c + o[1] * i < len(hidden[0])
-               and all(hidden[r + o[0] * i + dr][c + o[1] * i + dc] == "."
-                       for dr in range(-1, 2) for dc in range(-1, 2)
+    return all(0 <= r + o[0] * i < len(hidden) and 0 <= c + o[1] * i < len(hidden[0]) 
+               and all(hidden[r + o[0] * i + dr][c + o[1] * i + dc] == "." 
+                       for dr in range(-1, 2) for dc in range(-1, 2) 
                        if 0 <= r + o[0] * i + dr < len(hidden) and 0 <= c + o[1] * i + dc < len(hidden[0]))
                for i in range(size))
 
@@ -30,7 +30,7 @@ def take_turn(board, hidden):
             r, c = ord(shot[0]) - 65, int(shot[1:]) - 1
             if 0 <= r < len(board) and 0 <= c < len(board[0]) and board[r][c] == ".": break
         print("Invalid input, try again.")
-    if hidden[r][c] == "#":
+    if hidden[r][c] == "#": 
         board[r][c], hidden[r][c] = "X", "!"
         print("Hit!" if any("#" in row for row in hidden) else "You sunk the last ship!")
     else: board[r][c] = "O"; print("Miss!")
@@ -41,12 +41,14 @@ def play():
     board = [["."] * size for _ in range(size)]
     hidden = [["."] * size for _ in range(size)]
     input("Enter your name: ")
+    place_ships(hidden, ships)
     shots = 0
     while True:
-        clearTerminal(); print_board(board)
+        clear(); print_board(board)
         shots += 1
         if take_turn(board, hidden): break
-    clearTerminal(); print_board(board)
+    clear(); print_board(board)
     print(f"Victory in {shots} shots!")
 
-play()
+if name == "main":
+    play()
